84ebfa7400393b740a8258f805aff4d98cecfea1
llunaco <llunaco727@gmail.com>
2023-09-27 01:21:32 +0100

    favicon, page uniformity

--- js/script.js
+++ js/script.js
@@ -1,3 +1,28 @@
+class llunacoHeader extends HTMLElement {
+    connectedCallback() {
+        this.innerHTML = `
+        <header>
+    <a href="./index" class="logo">llunaco</a>                                              <!--title-->
+    <ul class="navlist">                                                                    <!--navbar links-->
+        <li><a href="./index">Home</a></li>
+        <li><a href="./creative">Creative</a></li>
+        <li><a href="./gaming">Gaming</a></li>
+        <li class="dropdown">
+            <a href="/profiles.html">Profiles</a>
+            <div class="dropdown-content">
+                <a href="#">Profile 1</a>
+                <a href="#">Profile 2</a>
+                <a href="#">Profile 3</a>
+            </div>
+        </li>
+    </ul>
+</header>
+        `
+    }
+}
+
+customElements.define('llunaco-header', llunacoHeader)
+
 let navlist = document.querySelector('.navlist');
 
 const sr = ScrollReveal ({
